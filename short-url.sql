28262ba61d89c4b690320dab79e5bca49215fcc4
jinxing.liu <821901008@qq.com>
2019-12-17 18:34:25 +0800

    feat: 短网址算法修改

--- short-url.sql
+++ short-url.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 本地MySQL
+ Source Server         : 我的本地连接
  Source Server Type    : MySQL
- Source Server Version : 80016
+ Source Server Version : 50721
  Source Host           : localhost:3306
  Source Schema         : short-url
 
  Target Server Type    : MySQL
- Target Server Version : 80016
+ Target Server Version : 50721
  File Encoding         : 65001
 
- Date: 12/12/2019 23:47:08
+ Date: 17/12/2019 18:33:53
 */
 
 SET NAMES utf8mb4;
@@ -30,7 +30,7 @@ CREATE TABLE `app` (
   `updated_at` datetime NOT NULL COMMENT '修改时间',
   PRIMARY KEY (`app_id`),
   UNIQUE KEY `app_key` (`app_key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='应用信息';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='应用信息';
 
 -- ----------------------------
 -- Table structure for app_url
@@ -39,11 +39,13 @@ DROP TABLE IF EXISTS `app_url`;
 CREATE TABLE `app_url` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `app_id` int(11) NOT NULL COMMENT '关联应用',
+  `short_id` varchar(100) NOT NULL DEFAULT '' COMMENT '短ID',
   `url` varchar(255) NOT NULL COMMENT '地址信息',
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `app_url` (`app_id`,`url`) COMMENT '应用url唯一'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='应用url';
+  UNIQUE KEY `app_url` (`app_id`,`url`) COMMENT '应用url唯一',
+  UNIQUE KEY `short_id` (`short_id`) USING BTREE COMMENT '短ID必须唯一'
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='应用url';
 
 SET FOREIGN_KEY_CHECKS = 1;
